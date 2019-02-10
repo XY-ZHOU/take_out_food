9e6dcb6c9dbbc9bc36d8a71aa6cf73fa5e0ad35b
XY-ZHOU <731986251@qq.com>
2019-02-10 16:25:41 +0800

    add object of item

--- src/Main/Item.java
+++ src/Main/Item.java
@@ -1,4 +1,31 @@
 package Main;
 
 public class Item {
+    private String id;
+    private String name;
+    private int count;
+    private int price;
+
+    public Item(String id, String name, int count, int price) {
+        this.id = id;
+        this.name = name;
+        this.count = count;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }
